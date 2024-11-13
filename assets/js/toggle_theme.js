64431a35c9e0d5648b06cdbab7b1824f1cc6d349
ycw790710 <70627808+ycw790710@users.noreply.github.com>
2024-11-13 18:03:25 +0800

    改成使用google icon source

--- assets/js/toggle_theme.js
+++ assets/js/toggle_theme.js
@@ -39,7 +39,7 @@
       const themeSwitcherText = document.querySelector('#bd-theme-text')
       const activeThemeIcon = document.querySelector('.theme-icon-active')
       const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
-      const svgOfActiveBtn = btnToActive.querySelector('img').getAttribute('src')
+      const svgOfActiveBtn = btnToActive.querySelector('span').innerText
   
       document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
         element.classList.remove('active')
@@ -48,7 +48,7 @@
   
       btnToActive.classList.add('active')
       btnToActive.setAttribute('aria-pressed', 'true')
-      activeThemeIcon.setAttribute('src', svgOfActiveBtn)
+      activeThemeIcon.innerText = svgOfActiveBtn
       const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`
       themeSwitcher.setAttribute('aria-label', themeSwitcherLabel)
   
